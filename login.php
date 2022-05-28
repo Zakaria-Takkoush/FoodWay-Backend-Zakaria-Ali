9100aab7272b1e8eccf322bff77f0fdc43c45751
Zakaria <zaktakk96@gmail.com>
2022-05-28 20:34:40 +0300

    Backend - Created login api

--- login.php
+++ login.php
@@ -1,3 +1,25 @@
 <?php
 
+include("connection.php"); // connected file to database
+
+$email = $_POST["email"];
+$password = hash("sha256", $_POST["password"]);
+
+$query = $mysqli->prepare("Select user_id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+$query->bind_result($id);
+$query->fetch();
+$response = [];
+if($num_rows == 0){
+    $response["response"] = "User Not Found";
+}else{
+    $response["response"] = "Logged in";
+    $response["user_id"] = $id;
+}
+$json == json_encode($response);
+echo $json;
 ?>
+
