39b02a8e2a102413cfd0b5c4701b489285071660
Zakaria <zaktakk96@gmail.com>
2022-05-29 15:51:50 +0300

    Backend - fixed add_resto api

--- add_resto.php
+++ add_resto.php
@@ -5,12 +5,12 @@ include("connection.php");
 $resto_name = $_POST["resto_name"];
 $phone_number = $_POST["phone_number"];
 $desc = $_POST["description"];
-$image = $_POST["image_url"];
+$image = $_POST["image"];
 $cat = $_POST["cat_id"];
 $city = $_POST["city_id"];
 
 
-$query = $mysqli->prepare("INSERT INTO restaurants(resto_name, phone_number, description, image_url, cat_id, city_id) VALUES (?, ?, ?, ?, ?, ?)");
+$query = $mysqli->prepare("INSERT INTO restaurants(resto_name, phone_number, description, image, cat_id, city_id) VALUES (?, ?, ?, ?, ?, ?)");
 $query->bind_param("ssssii", $resto_name, $phone_number, $desc, $image, $cat, $city);
 $query->execute();
 
