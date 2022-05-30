a1c4ba62a6c13665ec281d3723f14378ac37f537
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-30 22:41:40 +0300

    Backend - Added get ctity api

---
+++ get_city.php
@@ -0,0 +1,21 @@
+<?php
+
+include("connection.php");
+
+$city_id = $_POST["city_id"];
+
+$query = $mysqli->prepare("SELECT * from cities where city_id = ?");
+$query->bind_param("i",$city_id);
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+while($city = $array->fetch_assoc()){
+    $response[] = $city;
+} 
+
+$json = json_encode($response);
+echo $json;
+
+?>
