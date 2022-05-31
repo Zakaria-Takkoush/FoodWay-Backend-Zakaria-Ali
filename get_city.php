e4207fe7d31639010e1e4254b8f2e9e1d03a8e3c
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-31 14:20:35 +0300

    Backend - Added get category api

--- get_city.php
+++ get_city.php
@@ -1,11 +1,33 @@
 <?php
 
+// include("connection.php");
+
+// /*$city_id = $_POST["city_id"];*/
+
+// $query = $mysqli->prepare("SELECT city_name from cities /*where city_id = ?*/");
+// /*$query->bind_param("i",$city_id);*/
+// $query->execute();
+
+// $array = $query->get_result();
+
+// $response = [];
+// while($city = $array->fetch_assoc()){
+//     $response[] = $city;
+// } 
+
+// $json = json_encode($response);
+// echo $json;
+
+
+
+
+
 include("connection.php");
 
-/*$city_id = $_POST["city_id"];*/
+$resto_id = $_POST["resto_id"];
 
-$query = $mysqli->prepare("SELECT city_name from cities /*where city_id = ?*/");
-/*$query->bind_param("i",$city_id);*/
+$query = $mysqli->prepare("SELECT city_name FROM cities INNER JOIN restaurants ON restaurants.city_id=cities.city_id WHERE resto_id=?");
+$query->bind_param("i",$resto_id);
 $query->execute();
 
 $array = $query->get_result();
@@ -17,5 +39,7 @@ while($city = $array->fetch_assoc()){
 
 $json = json_encode($response);
 echo $json;
+
+
 
 ?>
