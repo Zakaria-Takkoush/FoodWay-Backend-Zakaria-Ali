dbd419ecec361d1c82529662689f548564a46ba8
Zakaria <zaktakk96@gmail.com>
2022-05-31 22:48:46 +0300

    Backend - Fixed edit_profile.php

--- edit_profile.php
+++ edit_profile.php
@@ -2,20 +2,58 @@
 
 include("connection.php");
 
-$f_name = $_POST["first_name"];
-$l_name = $_POST["last_name"];
-$gender = $_POST["gender"];
-$email = $_POST["email"];
-$password = hash("sha256", $_POST["password"]);
-$phone_number = $_POST["phone_number"];
-$image = $_POST["image_url"];
-$city = $_POST["city_id"];
+if(isset($_POST["user_id"])){
+    $user_id = $_POST["user_id"];
+} 
 
-$user_id = $_POST["id"];
+if(isset($_POST["first_name"])){
+    $f_name = $_POST["first_name"];
+} else {
+    die("Missing First Name!");
+}
+
+if(isset($_POST["last_name"])){
+    $l_name = $_POST["last_name"];
+} else {
+    die("Missing Last Name!");
+}
+
+if(isset($_POST["email"])){
+    $email = $_POST["email"];
+} else {
+    die("Missing email!");
+}
+
+if(isset($_POST["password"])){
+    $password = hash("sha256", $_POST["password"]);
+} else {
+    die("Missing password!");
+}
+
+// if(isset($_POST["phone_number"])){
+//     $phone_number = $_POST["phone_number"];
+// } else {
+//     die("Missing Phone Number!");
+// }
+
+if(isset($_POST["gender"])){
+    $gender = $_POST["gender"];
+} else {
+    die("Missing Gender!");
+}
+
+if(isset($_POST["city_id"])){
+    $city = $_POST["city_id"];
+} else {
+    die("Missing City!");
+}
+
+
+// $is_admin = 0;
 
 $query = $mysqli->prepare("UPDATE `users` SET `first_name`= ?,`last_name`= ?,`gender`= ?,
-`email`= ?,`password`= ?,`image`= ?, `city_id` = ? WHERE `user_id` = ?");
-$query->bind_param("ssisssi",$f_name ,$l_name,$gender,$email,$password,$phone_number,$image,$city);
+`email`= ?,`password`= ?, `city_id` = ? WHERE `user_id` = ?");
+$query->bind_param("ississi",$user_id, $f_name ,$l_name,$gender,$email,$password,$city);
 $query->execute();
 
 $response = [];
--- get_user_city.php
+++ get_user_city.php
@@ -2,9 +2,12 @@
 
 include("connection.php");
 
-$user_id = $_POST["user_id"];
+if(isset($_POST["id"])){
+    $user_id = $_POST["id"];
+}
 
 $query = $mysqli->prepare("SELECT cities.city_name FROM cities,users WHERE users.city_id = cities.city_id and users.user_id = ?");
+//$query = $mysqli->prepare("SELECT city_name FROM cities INNER JOIN users ON users.city_id=cities.city_id WHERE user_id=?");
 $query->bind_param("i",$user_id);
 $query->execute();
 
@@ -15,8 +18,9 @@ while($city = $array->fetch_assoc()){
     $response[] = $city;
 } 
 
+$response["success"] = 'Sucess!';  
+
 $json = json_encode($response);
 echo $json;
-
 
 ?>
