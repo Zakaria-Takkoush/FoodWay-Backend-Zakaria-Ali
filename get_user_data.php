4ba81b3ed46141151777f9bbc0108e3dcfdec317
Zakaria <zaktakk96@gmail.com>
2022-05-31 18:28:32 +0300

    Backed - created get_user_data api

---
+++ get_user_data.php
@@ -0,0 +1,23 @@
+<?php
+
+include("connection.php");
+
+$user_id= $_POST["id"];
+
+$query = $mysqli->prepare("SELECT * from users WHERE user_id = ?");
+$query->bind_param("i",$user_id);
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+while($user = $array->fetch_assoc()){
+    $response[] = $user;
+} 
+
+$response["success"] = 'Data Collected';  
+
+$json = json_encode($response);
+echo $json;
+
+?>
